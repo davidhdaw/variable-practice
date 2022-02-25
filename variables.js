dc1a7ee42adc0a5d4b4c6feb61391e9a3055e725
David Daw <daviddaw@gmail.com>
2022-02-24 19:06:09 -0800

    add boolean variable examples and a bad Screamo joke

--- variables.js
+++ variables.js
@@ -5,3 +5,11 @@ var artistName = "Open Mike Eagle";
 var dogAge = 8;
 var dogDailyMeals = 2;
 var dogScreamCount = 391;
+
+var dogScream = true;
+var wifeScream = false;
+var artistScream = false;
+
+artistName = "He Is Legend";
+artistScream = true;
+var badScreamoJoke = true;
