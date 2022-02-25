51ca7acf4baf3a975697207f64bfded16a199d5f
David Daw <daviddaw@gmail.com>
2022-02-24 19:13:06 -0800

    annotates the boolean variable section
    Also annotates the terrible screamo joke and corresponding variable.

--- variables.js
+++ variables.js
@@ -1,3 +1,4 @@
+
 var dogName = "Oscar";
 var wifeName = "Nikki";
 var artistName = "Open Mike Eagle";
@@ -6,10 +7,18 @@ var dogAge = 8;
 var dogDailyMeals = 2;
 var dogScreamCount = 391;
 
+//Declares a variable dogScream and sets it as a boolean of true.
 var dogScream = true;
+//Declares a variable wifeScream and sets it as a boolean of false.
 var wifeScream = false;
+//Declares a variable artistScream and sets it as a boolean of false.
 var artistScream = false;
 
+//Reassigns the artistName variable to the string "He Is Legend" then reassigns the artistScream
+//variable to true because they are a screamo band so they scream. I've never listened to them so I don't know
+//f they're any good I just Googled for screamo bands.
 artistName = "He Is Legend";
 artistScream = true;
+
+//Declares a badScreamoJoke variable and assigns it to the boolean true to confirm that joke was not worth the effort.
 var badScreamoJoke = true;
